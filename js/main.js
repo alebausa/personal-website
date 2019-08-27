0a9a449f5b4b6fef111a7da03247b957dba8196f
Ale Bausa <ale.bausa@gmail.com>
2019-08-27 17:20:06 +0200

    add icons and footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-var sentences = ["The latest HTML5 accessible standards", "Progressive Web Apps", "Single Page Apps", "Improve your brand image through SEO and SEM positioning","Community managing and brand positioning", "Agile developing"];
+var sentences = [
+  "UX/UI design",
+  "The latest HTML5 accessible standards",
+  "Progressive Web Apps developement",
+  "Fast and optimized Single Page Apps",
+  "Improve your brand image through SEO and SEM positioning",
+  "Community managing and brand positioning",
+  "Agile developing"
+];
 
-function getRandomSentence(arr){
+function getRandomSentence(arr) {
   let phrase = arr[Math.floor(Math.random() * arr.length)];
   document.getElementById("randomSentence").innerText = phrase;
 }
